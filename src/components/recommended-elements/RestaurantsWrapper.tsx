9d0354bf4753d227704414311c9edc1d2469fda1
unknown <xlongclaw@gmail.com>
2024-02-18 20:13:40 +0530

    redesigned Restaurant Wrapper

--- src/components/recommended-elements/RestaurantsWrapper.tsx
+++ src/components/recommended-elements/RestaurantsWrapper.tsx
@@ -1,23 +1,52 @@
 import React from "react";
+import {
+  BsClock,
+  BsDot,
+  BsHeart,
+  BsMenuApp,
+  BsMenuAppFill,
+  BsStar,
+  BsStarFill,
+  BsThreeDots,
+} from "react-icons/bs";
 import { FiStar } from "react-icons/fi";
 
-const RestaurantsWrapper: React.FC<{name:string;imgloc:string;location:string;rating:number}> = (props) => {
+const RestaurantsWrapper: React.FC<{
+  name: string;
+  imgloc: string;
+  location: string;
+  rating: number;
+}> = (props) => {
   return (
     <div
-      className={`${props.imgloc} text-white hover:scale-110 transition-all duration-700 w-[14rem] h-[20rem] sm:w-[16rem] sm:h-[25rem] md:w-[18rem] md:h-[28rem] lg:w-[22rem] lg:h-[32rem] bg-cover rounded-tl-none rounded-3xl flex overflow-hidden flex-col justify-end`}
+      className={`${props.imgloc} text-white hover:scale-110 transition-all duration-700  relative
+      w-[14rem] h-[20rem] sm:w-[16rem] sm:h-[25rem] md:w-[18rem] md:h-[28rem] lg:w-[22rem]
+       lg:h-[32rem] bg-cover rounded-tl-none rounded-3xl flex overflow-hidden flex-col justify-end`}
     >
-      <div className="h-[7rem] bg-color1/60 px-8 md:px-12 py-4 flex flex-col justify-center gap-1">
-        <div className="flex w-full justify-between text-sm sm:text-base md:text-xl font-extrabold">
-          <h2 className="">{props.name}</h2>
-          <h2 className=" flex gap-2 items-center">
-            <FiStar />
+      <div className="h-[7rem] bg-color1/60 px-8 md:px-6 py-4 flex flex-col justify-center gap-1">
+        <div className="flex w-full justify-between items-center text-sm sm:text-base md:text-xl font-extrabold">
+          <div>
+            <h2 className="">{props.name}</h2>
+            <div className="text-sm md:text-sm font-medium md:font-semibold text-white/80">
+              {/* locaionico */}
+              {props.location}
+            </div>
+            <div className="flex text-xs gap-0 items-center font-medium mt-2">
+              <BsClock size={12} />
+              <h3 className="ml-2">25-30 min</h3>
+              <BsDot size={20} />
+              <h3>2 km </h3>
+            </div>
+          </div>
+          <h2 className=" flex gap-1 text-xs items-center h-[2rem] bg-[#16a034] px-4 rounded-md">
+            <BsStarFill />
             {props.rating}
           </h2>
         </div>
-        <div className="text-sm md:text-base font-medium md:font-semibold">
-          {/* locaionico */}
-          {props.location}
-        </div>
+      </div>
+      <div className="top-0 absolute right-0 p-6 flex gap-4">
+        <BsHeart />
+        <BsThreeDots />
       </div>
     </div>
   );
--- src/screens/homepage-sections/RecommendedSection.tsx
+++ src/screens/homepage-sections/RecommendedSection.tsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { RestaurantsWrapper } from "../../components/recommended-elements";
 import { restaurantsData } from "../../constants";
+import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 
 const RecommendedSection: React.FC = () => {
   return (
-    <div className="pl-16 sm:pl-20 md:pl-32 flex flex-col gap-12">
-      <h3 className="bold-font text-3xl md:text-4xl xl:text-5xl text-color1 ">
-        Recommended Restaurants
-      </h3>
-      <div className="py-8 grid grid-flow-col gap-8 md:gap-16 overflow-x-auto hidescroll">
+    <div className="pl-16 sm:pl-20 md:pl-32 flex flex-col relative">
+      <div className="flex justify-between mr-10 items-center">
+        <div>
+          <h3 className="bold-font text-3xl md:text-4xl xl:text-5xl text-color1 ">
+            Recommended Restaurants
+          </h3>
+          <h4 className=" font-semibold text-xl text-color1/80 mb-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa,
+            voluptatum?
+          </h4>
+        </div>
+        <div className="flex text-xl gap-4 items-center font-semibold">
+          <h1 className="hover:underline cursor-pointer">See more</h1>
+          <BsArrowRight size={30} />
+        </div>
+      </div>
+      <div className="py-8 grid grid-flow-col gap-8 md:gap-8 overflow-x-auto hidescroll pb-8">
         {restaurantsData.map((restaurant) => (
           <>
             <RestaurantsWrapper
@@ -19,6 +32,17 @@ const RecommendedSection: React.FC = () => {
             />
           </>
         ))}
+      </div>
+      <div className="flex gap-6 justify-end mr-16 mt-8">
+        {" "}
+        <BsArrowLeft
+          className="bg-white border-2 border-color1 p-6 text-color1 rounded-2xl"
+          size={80}
+        />{" "}
+        <BsArrowRight
+          className="p-6 bg-color1 text-white rounded-2xl"
+          size={80}
+        />{" "}
       </div>
     </div>
   );
